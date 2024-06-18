1547905cd64f735e7ee4161ad7a8cffd847f89be
sk <sandipank.dev@gmail.com>
2024-06-19 00:44:49 +0530

    initial pages content

--- app/layout.config.tsx
+++ app/layout.config.tsx
@@ -1,10 +1,13 @@
 import { type BaseLayoutProps } from 'fumadocs-ui/layout';
 
+
 // basic configuration here
 export const baseOptions: BaseLayoutProps = {
+  
   nav: {
     title: 'Shardeum Documentation',
   },
+  
   links: [
     {
       text: 'Documentation',
